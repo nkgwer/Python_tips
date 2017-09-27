b8bdad52dc02521dee99ad8aec4a38f6224fd0d5
Yota Nakagawa <yota.nakagawa@suou.waseda.jp>
2017-09-27 11:52:08 +0900

    set

---
+++ set.py
@@ -0,0 +1,17 @@
+numbers = {1, 2, 3, 4, 5, 5}
+print(numbers)
+print(type(numbers))
+
+animals = set(["kobuta", "tanuki", "kitsune", "neko"])
+print(animals)
+print(type(animals))
+
+list_a = [1, 1, 12, 3, 3, 3, 3, 3, 3, 4]
+list_b = [4, 3, 1, 12]
+
+print(set(list_a) == set(list_b))
+
+print(set(list_a) is set(list_b))
+
+for element in animals:
+    print(element)
