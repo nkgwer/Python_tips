3c33e9d2995869554dc11d0bca003fabec4f540a
Yota Nakagawa <yota.nakagawa@suou.waseda.jp>
2017-09-28 08:12:32 +0900

    set

--- import_1.py
+++ import_1.py
@@ -1,3 +1,4 @@
+# https://qiita.com/suzuki-hoge/items/f951d56290617df4279e#from%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E6%9B%B8%E3%81%8F
 import import_package.module1
 
 import_package.module1.print_hello()
