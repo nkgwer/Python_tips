e981250d243d53e99403736126ffad0df4802a21
Yota Nakagawa <yota.nakagawa@suou.waseda.jp>
2017-09-29 08:23:55 +0900

    csv

--- csv_file.py
+++ csv_file.py
@@ -3,4 +3,8 @@ with open('test.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile)
     for element in spamreader:
         print(element)
-        
+
+with open('USjudgeRatings.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile)
+    for element in spamreader:
+        print("".join(element))
