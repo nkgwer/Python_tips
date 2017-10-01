7cf93050e4b63c2c19b359468e5b4d8215175547
Yota Nakagawa <yota.nakagawa@suou.waseda.jp>
2017-10-02 06:09:48 +0900

    slice

---
+++ slice.py
@@ -0,0 +1,10 @@
+l = [1, 2, 3, 4, 5]
+print(l[3:4])
+#print(l[x,y]) returns l[x],l[x+1]... l[y-1]
+
+print(l[::2])
+print(l[::-1])
+
+print(l[:-1:]) # excludes the last element
+
+print("apples"[:-1]) #like this
